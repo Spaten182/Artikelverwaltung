0611ce9eba0fab75fd0a87386ad416cd3cf7ec24
NEisenbarth <48549735+NEisenbarth@users.noreply.github.com>
2019-03-14 12:33:52 +0100

    Views added
    -ViewCreate
    -ViewEdit

--- View/ViewCreate.java
+++ View/ViewCreate.java
@@ -8,12 +8,12 @@
  *
  * @author nikla
  */
-public class ViewEdit extends javax.swing.JFrame {
+public class ViewCreate extends javax.swing.JFrame {
 
     /**
      * Creates new form EditWindow
      */
-    public ViewEdit() {
+    public ViewCreate() {
         initComponents();
     }
 
@@ -145,13 +145,13 @@ public class ViewEdit extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ViewEdit.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ViewCreate.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ViewEdit.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ViewCreate.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ViewEdit.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ViewCreate.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ViewEdit.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ViewCreate.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
         //</editor-fold>
@@ -159,7 +159,7 @@ public class ViewEdit extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ViewEdit().setVisible(true);
+                new ViewCreate().setVisible(true);
             }
         });
     }
